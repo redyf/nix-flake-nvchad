6840fae9915b105eea864f004ebbbcbe36e11242
Redyf <mateusalvespereira7@gmail.com>
2024-02-17 23:00:57 -0300

    feat: Remove flake from gitignore

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "NvChad's Neovim Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    ...
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        inherit (nixpkgs) lib;
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        nvim =
+          pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped
+          (pkgs.neovimUtils.makeNeovimConfig
+            {
+              customRC = ''
+                set runtimepath^=${./.}
+                source ${./.}/init.lua
+              '';
+            }
+            // {
+              wrapperArgs = [
+                "--prefix"
+                "PATH"
+                ":"
+                "${lib.makeBinPath [pkgs.gcc pkgs.nil pkgs.chafa pkgs.ffmpegthumbnailer pkgs.wakatime pkgs.statix]}"
+              ];
+            });
+      in {
+        overlays = {
+          neovim = _: _prev: {
+            neovim = nvim;
+          };
+          default = self.overlays.neovim;
+        };
+
+        packages = rec {
+          neovim = nvim;
+          default = neovim;
+        };
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.stylua
+          ];
+        };
+      }
+    );
+}
